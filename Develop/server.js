a0c86d1b1bb61087ca18b7bd72bccfa79d47e926
nathanielmuci <nate.muci@gmail.com>
2022-11-30 22:04:55 -0600

    added routes

---
+++ Develop/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
